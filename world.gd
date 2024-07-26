b06d3f9987b907bf2d93ff7d85157195bcbf6325
DeathMagnet <death.magnet@gmail.com>
2024-07-25 19:36:15 -0700

    add-launch-button

--- world.gd
+++ world.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
 @onready var animation_player = $AnimationPlayer
+@onready var launch_button = $LaunchButton
+@onready var rocket_flare = $Rocket/RocketFlare
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _on_launch_button_button_down() -> void:
 	animation_player.play("launch")
-	print("Hello, World!")
+	rocket_flare.show()
+	launch_button.hide()
